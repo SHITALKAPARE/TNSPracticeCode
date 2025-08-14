b6092593f6c7bb3fc78e4cd8c729d9922d4f9966
DELL <DELL@DESKTOP-OT0HLG6>
2025-08-14 21:13:44 +0530

    Exception Handling

---
+++ ExceptionHandling/src/com/exceptionhandling/TryCatch.java
@@ -0,0 +1,22 @@
+package com.exceptionhandling;
+
+public class TryCatch {
+
+	public static void main(String[] args) {
+
+
+		int arr[] = {1,2,3,4,5};
+		try {
+			System.out.println(arr[9]);
+		}
+		catch(ArrayIndexOutOfBoundsException a)
+		{
+			System.out.println(a.getMessage());
+			
+		}
+		
+	}
+
+}
+
+
---
+++ ExceptionHandling/src/com/userdefinedExceptions/InvalidMarksException.java
@@ -0,0 +1,21 @@
+package com.userdefinedExceptions;
+
+public class InvalidMarksException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+	
+}
+
+
+	
