b8c0d9eca8ec980c9aa27528cce7bfb33dfac3aa
DELL <DELL@DESKTOP-OT0HLG6>
2025-08-14 21:19:40 +0530

    SingleInheritance

---
+++ SingleInheritance/src/com/singleinheritance/SLInheritance.java
@@ -0,0 +1,10 @@
+package com.singleinheritance;
+
+public class SLInheritance {
+	public static void main(String[] args) {
+
+		Student stu = new Student("Gayatri", 675547823456L, "Pune",9876543212L,101,"JSPM College");
+		
+		System.out.println(stu);
+	}
+}
