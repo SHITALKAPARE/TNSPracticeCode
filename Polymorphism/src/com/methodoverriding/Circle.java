0445db1b791b38afd0f8343c7fd24d7d8d5756e9
DELL <DELL@DESKTOP-OT0HLG6>
2025-08-14 21:17:25 +0530

    Polymorphism

---
+++ Polymorphism/src/com/methodoverriding/Circle.java
@@ -0,0 +1,29 @@
+package com.methodoverriding;
+
+public class Circle extends Shape{
+
+	private double radius;
+
+	// constructor for circle
+	public Circle(double radius) {
+		super();
+		this.radius = radius;
+	}
+	
+	// override draw method for circle
+	
+	@Override 
+	public void draw()
+	{
+		System.out.println("Drawing a circle with radius "+ radius);
+	}
+	
+	
+	// override erase method for circle
+	
+		@Override 
+		public void erase()
+		{
+			System.out.println("Erasing a circle with radius "+ radius);
+		}
+}
