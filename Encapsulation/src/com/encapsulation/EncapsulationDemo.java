9286f843834aca90b0b5f6c03f4e61364ea63121
DELL <DELL@DESKTOP-OT0HLG6>
2025-08-14 21:12:46 +0530

    Encapsulation

---
+++ Encapsulation/src/com/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,18 @@
+package com.encapsulation;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+
+		OopsConceptDemo obj = new OopsConceptDemo();
+		
+		obj.setSerialNum(101);
+		obj.setName("Sagar");
+		obj.setAge(22);
+		
+		System.out.println(obj);
+		
+	}
+
+}
+
+
